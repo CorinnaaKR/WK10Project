e327cb9c58c93386cbd92515a8aeb8e7f60d2d61
Corinna <x.corinna.x@hotmail.co.uk>
2025-10-15 16:19:34 +0100

    small changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,10 @@ import TestLayout from "@/components/TestLayout";
 export default function Homepage() {
   return (
     <>
+      
       <TestLayout>
         <h1>Home Page</h1>
-      </TestLayout>
+        </TestLayout>
     </>
   );
 }
