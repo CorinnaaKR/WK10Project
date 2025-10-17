b50440adbe18435e6e6b7cc1b8e42c5e76d17f85
Steph <steph@polyhydral.co.uk>
2025-10-17 10:41:31 +0100

    final pre presentation push

--- src/components/TestLayout.jsx
+++ src/components/TestLayout.jsx
@@ -1,8 +1,16 @@
 "use client";
 import { Flex, Layout } from "antd";
 import "antd/dist/reset.css"; // for AntD v5
+import DemoList from "@/components/List/demo-translated.js";
 import VeryLongNoticeBar from "@/components/NoticeBar/demo-translated.js";
+import NavBar from "@/components/NavBar/demo-translated.js";
+import { WelcomeTextDemo } from "@/components/TextArea/demo-translated.js";
+import DividerDemo from "@/components/Dividers/divider.js";
 import WelcomeAreaText from "@/components/CentralisedText/welcome-area-text.js";
+import ProjectsTabs from "@/components/Tabs/projects-tabs.js";
+import Gallery from "@/components/Gallery/gallery.js";
+import ProjectFooter from "@/components/footer/footer.js";
+import GitHubText from "@/components/CentralisedText/githubtext.js";
 
 const { Header, Sider, Content, Footer } = Layout;
 
@@ -18,10 +26,19 @@ const TestLayout = () => (
       <Header
         style={{ background: "peru", color: "white", textAlign: "center" }}
       >
-        Header
+        Corinna & Steph's Week 10 Project
       </Header>
       <Content style={{ padding: "24px" }}>
         <WelcomeAreaText />
+        <DemoList />
+      <ProjectsTabs />
+      <DividerDemo />
+      <WelcomeTextDemo />
+      <DividerDemo />
+      <GitHubText />
+      <Gallery />
+      <DividerDemo />
+      <ProjectFooter />
       </Content>
       <Footer style={{ textAlign: "center" }}>Footer</Footer>
     </Layout>
